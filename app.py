a109c506f2b31c41a9aba9710cb17ea83f2d6ffb
Mikko Paavola <mikko.paavola@helsinki.fi>
2024-11-11 15:40:12 +0200

    feature: add db connection and a message of day in the front page to test the functionality

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:admin@localhost"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    result = db.session.execute(text("SELECT content FROM messages"))
+    messages = result.fetchall()
+    print(messages)
+    return render_template("index.html", messages=messages) 
