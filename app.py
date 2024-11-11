a75897492d51e6df6feffbd839ce56266d773007
Mikko Paavola <mikko.paavola@helsinki.fi>
2024-11-11 16:02:43 +0200

    build: add loading of webapp secret key from a separate env-file

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from os import getenv
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
+
+# Load secret from an separate .env file
+load_dotenv(".secret_env")
+app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
