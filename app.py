53d245ccd756736a69a28e0d418c0f53566ec3c5
Mikko Paavola <mikko.paavola@helsinki.fi>
2024-11-28 18:39:19 +0200

    build: remove all env files from repository

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from os import getenv
-from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 import src.routes
 
-# Load secret from an separate .env file
-load_dotenv(".secret_env")
 app.secret_key = getenv("SECRET_KEY")
